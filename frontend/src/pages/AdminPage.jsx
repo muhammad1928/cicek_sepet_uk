1fb30adc403f62f88ffbea6a2618e7f7325c5192
muhammad1928 <muhammadsn7@gmail.com>
2025-12-05 11:58:06 +0100

    production

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -36,7 +36,7 @@ const AdminPage = () => {
 
   return (
  // pt-20: Navbar yüksekliği kadar boşluk (Navbar fixed olduğu için)
-    <div className="min-h-screen flex bg-gray-100 pt-20 font-sans ">
+    <div className="min-h-screen flex bg-gray-100 font-sans ">
       
       {/* --- SIDEBAR (SOL MENÜ) --- */}
       {/* fixed: Sayfa kaydırılsa bile yerinde durur */}
